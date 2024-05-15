cdc51e37ae6f313f3e62a1fdac70f42defe206ff
Janek Janczewski <iJanczewskij@gmail.com>
2024-05-15 18:44:20 +0200

    dodane pomysły 15.05

---
+++ main_functions.py
@@ -0,0 +1,14 @@
+import os
+from time import sleep
+
+
+def clear():
+    if os.name == "posix":
+        os.system("clear")
+    else:
+        os.system("cls")
+
+def sleep_and_clear(time):
+    sleep(time)
+    clear()
+
