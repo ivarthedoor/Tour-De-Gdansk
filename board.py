ac227c3f5a3bbc6899d83fe97699435e55d874b8
ivart <ivar.the.door@gmail.com>
2024-05-15 19:35:12 +0200

    pracuje nad planszą

--- board.py
+++ board.py
@@ -1,17 +1,25 @@
-x = 0
-board_base = []
-def board(x):
-    while x < 100:
-        x += 1
-        board_base.append(str(x))
-        if x % 10 == 0:
-            board_base.append("\n")
-board(x)
+# #import movement_programms
+
+# def board(board_base = None):
+#     if board_base is None:
+#         board_base = []
+#     x = 0
+#     while x < 10:
+#         x += 1
+#         board_base.append(str(x))
+#     board_lenght = len(board_base)
+#     empty = ".".join(["____" for characters in board_base])
+#     return empty
+
+# print(board())
 
 
-def clear_gameboard():
-    game_board = "|".join(board_base)
-    board_lenght = len(game_board)
-    empty = ".".join(["_" for characters in game_board])
-    print("Nazwa: " + "".join(empty))
-clear_gameboard()
+
+# def example_player():
+#     position = 0
+
+# def example_district():
+#     players_position = board()
+plansza = [1, 2, 3, 4]
+print(plansza)
+print()
