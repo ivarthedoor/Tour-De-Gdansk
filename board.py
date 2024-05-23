3f20d0e1eff97c68faf62766ea55a8945e52634d
Pawel <pawel.ropela01@gmail.com>
2024-05-23 18:15:47 +0200

    Paweł testowy

--- board.py
+++ board.py
@@ -1,25 +1,25 @@
-# #import movement_programms
+#import movement_programms
 
-# def board(board_base = None):
-#     if board_base is None:
-#         board_base = []
-#     x = 0
-#     while x < 10:
-#         x += 1
-#         board_base.append(str(x))
-#     board_lenght = len(board_base)
-#     empty = ".".join(["____" for characters in board_base])
-#     return empty
+def board(board_base = None):
+    if board_base is None:
+        board_base = []
+    x = 0
+    while x < 10:
+        x += 1
+        board_base.append(str(x))
+    board_lenght = len(board_base)
+    empty = ".".join(["____" for characters in board_base])
+    return empty
 
-# print(board())
+print(board())
 
 
 
-# def example_player():
-#     position = 0
+def example_player():
+    position = 0
 
-# def example_district():
-#     players_position = board()
-plansza = [1, 2, 3, 4]
-print(plansza)
-print()
+def example_district():
+    players_position = board()
+# plansza = [1, 2, 3, 4]
+# print(plansza)
+# print()
