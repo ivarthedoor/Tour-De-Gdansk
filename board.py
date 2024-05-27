ec617ac1f2a8cc2c714bf37f16cf9dfe6f93a7bc
OlekCydejko <olekcydejko98@gmail.com>
2024-05-27 19:33:21 +0200

    update 27.05

--- board.py
+++ board.py
@@ -56,13 +56,67 @@ def make_players():
         print(f"{players_list[0]}.{players_list[1]}: {players_points[i]}")
     
 
-def game(): #nazwał bym to coś w stylu game_board_srodmieście itp, trzeba by wtedy dopisać, jak program ma się zakończyć i dopiero
+def game1(): #nazwał bym to coś w stylu game_board_srodmieście itp, trzeba by wtedy dopisać, jak program ma się zakończyć i dopiero
+    #finalnie w funkcji game, wywołał 4 takie funkcje jak obecna f-cja game.
+    global task_assingment_positions, position_A, position_B, position_C, position_D
+    is_game_on = True
+    while True:
+        make_players()
+        print(f"START.{generate_board()} \n \
+    ____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.META")
+        play = input("press 1 to move Blue, 2 to move Red, 3 to move Green, 4 to move Yellow or q to quit: ")
+        play = play.lower()
+
+        if play == "1":
+            position_A = move_player(position_A)
+            questions_assingment(position_A, 0)
+            print("Time for Red (2)")
+            sleep_and_clear(1)
+
+        elif play == "2":
+            position_B = move_player(position_B)
+            questions_assingment(position_B, 1)
+            print("Time for Green (3)")
+            sleep_and_clear(1)
+
+        elif play == "3":
+            position_C = move_player(position_C)
+            questions_assingment(position_C, 2)
+            print("Time for Yellow (4)")
+            sleep_and_clear(1)
+        elif play == "4":
+            position_D = move_player(position_D)
+            questions_assingment(position_D, 3)
+            print("Time for Blue (1)")
+            sleep_and_clear(1)
+        elif play == "q":
+            sleep_and_clear(1)
+            is_game_on = False
+            return is_game_on
+        else:
+            print("Invalid input! Please press 1, 2, 3, 4 or q.")
+
+        if position_A == 29 or position_B == 29 or position_C == 29 or position_D == 29:
+            print(generate_board())
+            if position_A == 29 or position_B == 29 or position_C == 29 or position_D == 29:
+                position_A = 0
+                position_B = 0
+                position_C = 0
+                position_D = 0
+                sleep_and_clear(0.01)
+                play = True
+                return play
+            
+
+
+def game2(): #nazwał bym to coś w stylu game_board_srodmieście itp, trzeba by wtedy dopisać, jak program ma się zakończyć i dopiero
     #finalnie w funkcji game, wywołał 4 takie funkcje jak obecna f-cja game.
     global task_assingment_positions, position_A, position_B, position_C, position_D
 
     while True:
         make_players()
-        print("START." + generate_board() + ".META")
+        print(f"START.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.____.\n \
+    {generate_board()}.META")
         play = input("press 1 to move Blue, 2 to move Red, 3 to move Green, 4 to move Yellow or q to quit: ")
         play = play.lower()
 
@@ -104,6 +158,16 @@ def game(): #nazwał bym to coś w stylu game_board_srodmieście itp, trzeba by 
                 print("Player C wins!")
             elif position_D == 29:
                 print("Player D wins!")
-            break
+            sleep_and_clear(1)
+            play = False
+            return play
 
-game()
+def initial_game(): 
+
+        game1()
+        game2()
+
+initial_game()
+
+print("Koniec")
+    
