7b2503692d3319f1ea2c2357df85d6ed9a2573c0
ivart <ivar.the.door@gmail.com>
2024-12-08 12:19:14 +0100

    ...

--- core.py
+++ core.py
@@ -2,49 +2,49 @@ from main_functions import sleep_and_clear
 from board import GameBoard, get_board_range
 from movement_programms import Player_move
 
-players_positions = [0, 0, 0, 0]
-task_assingment_positions = [i - 1 for i in get_board_range() if i % 5 == 0] # Co piąte pole 
-players_points = [0, 0, 0, 0]
+# players_positions = [0, 0, 0, 0]
+# task_assingment_positions = [i - 1 for i in get_board_range() if i % 5 == 0] # Co piąte pole 
+# players_points = [0, 0, 0, 0]
 class GameCore:
-    def __init__(self, players_positions, task_assingment_positions, players_points):
-        self.players_positions = players_positions
-        self.task_assingment_positions = task_assingment_positions
-        self.players_points = players_points
+    def __init__(self):
+        self.players_positions = [0, 0, 0, 0]
+        self.task_assingment_positions = [i - 1 for i in get_board_range() if i % 5 == 0]
+        self.players_points = [0, 0, 0, 0]
 
 
-    def position_reset():
-        players_positions[0] = 0
-        players_positions[1] = 0
-        players_positions[2] = 0
-        players_positions[3] = 0
+    def position_reset(self):
+        self.players_positions[0] = 0
+        self.players_positions[1] = 0
+        self.players_positions[2] = 0
+        self.players_positions[3] = 0
 
-    def next_level_points(a, b, c, d):
-        players_points[a] += 15
-        players_points[b] += 5
-        players_points[c] += 5
-        players_points[d] += 5
+    def next_level_points(self, a: int, b: int, c: int, d: int):
+        self.players_points[a] += 15
+        self.players_points[b] += 5
+        self.players_points[c] += 5
+        self.players_points[d] += 5
 
-    def questions_assingment(position, points_index):  # Losowanie pytania co piąte pole 
+    def questions_assingment(self, position, points_index):  # Losowanie pytania co piąte pole 
         from questions import abcd_questions
-        for i in task_assingment_positions:
+        for i in self.task_assingment_positions:
             if position == i:
                 if abcd_questions():
                     sleep_and_clear(3)
-                    players_points[points_index] += 10
+                    self.players_points[points_index] += 10
                 else:
                     sleep_and_clear(3)
-                    if players_points[points_index] != 0:
-                        players_points[points_index] -= 5
-        return players_points[points_index]
+                    if self.players_points[points_index] != 0:
+                        self.players_points[points_index] -= 5
+        return self.players_points[points_index]
 
-    def make_players(): # Generowanie graczy - ale jeszcze bez wyboru  ich ilosći 
+    def make_players(self): # Generowanie graczy - ale jeszcze bez wyboru  ich ilosći 
         players = [x for x in enumerate(["Blue", "Red","Green", "Yellow"], start=1)]
         i = 0
         for i in range(len(players)):
             a, b = players[i]
             players_list = a, b
-            players_board = players_list, players_points[i]
-            print(f"{players_list[0]}.{players_list[1]}: {players_points[i]}")
+            players_board = players_list, self.players_points[i]
+            print(f"{players_list[0]}.{players_list[1]}: {self.players_points[i]}")
 
 class GameLevels:        
     def game1(): # Dzielnica 1
