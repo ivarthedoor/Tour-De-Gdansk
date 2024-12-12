60d326cf55e76e506b55a34b08772fb726544bc0
ivart <ivar.the.door@gmail.com>
2024-12-12 20:10:16 +0100

    klasa dodająca dane do csv

--- core.py
+++ core.py
@@ -1,4 +1,4 @@
-from main_functions import sleep_and_clear, BOARD_RANGE, PLAYERS_POSITIONS
+from main_functions import sleep_and_clear, BOARD_RANGE, PLAYERS_POSITIONS, PLAYERS_POINTS
 from questions_reading import questions_dispenser
 
 class GameCore():
@@ -6,11 +6,11 @@ class GameCore():
         # Pobranie nazw graczy
         # self.players_positions = [0, 0, 0, 0]
         self.task_assingment_positions = [i - 1 for i in BOARD_RANGE if i % 5 == 0] # Co piąte pole 
-        self.players_points = [0, 0, 0, 0]
         self.blue = (f"\U0001F535 - {input("Wprowadź nazwę niebieskiego gracza: ")}")
         self.red = (f"\U0001F534 - {input("Wprowadź nazwę czerwonego gracza: ")}")
         self.green = (f"\U0001F7E2 - {input("Wprowadź nazwę zielonego gracza: ")}")
         self.yellow = (f"\U0001F7E1 - {input("Wprowadź nazwę żółtego gracza: ")}")
+        self.player_data = None
         sleep_and_clear(1)
     
     def position_reset(self): 
@@ -22,10 +22,10 @@ class GameCore():
 
     def next_level_points(self, a: int, b: int, c: int, d: int):
     # Punktacja graczy za poprawną odpowiedź 
-        self.players_points[a] += 15
-        self.players_points[b] += 5
-        self.players_points[c] += 5
-        self.players_points[d] += 5
+        PLAYERS_POINTS[a] += 15
+        PLAYERS_POINTS[b] += 5
+        PLAYERS_POINTS[c] += 5
+        PLAYERS_POINTS[d] += 5
 
     def questions_assingment(self, position: int, points_index: int, district: str)->int:  
     # Losowanie pytania co piąte pole 
@@ -33,12 +33,12 @@ class GameCore():
             if position == i:
                 if questions_dispenser(district):
                     sleep_and_clear(3)
-                    self.players_points[points_index] += 10
+                    PLAYERS_POINTS[points_index] += 10
                 else:
                     sleep_and_clear(3)
-                    if self.players_points[points_index] != 0:
-                        self.players_points[points_index] -= 5
-        return self.players_points[points_index]
+                    if PLAYERS_POINTS[points_index] != 0:
+                        PLAYERS_POINTS[points_index] -= 5
+        return PLAYERS_POINTS[points_index]
     
     def make_players(self): 
     # Generowanie graczy 
@@ -47,4 +47,8 @@ class GameCore():
         for i in range(len(players)):
             a, b = players[i]
             players_list = a, b
-            print(f"{players_list[0]}.{players_list[1]}: {self.players_points[i]}\n")
+            self.player_data = (f"{players_list[0]}.{players_list[1]}: {PLAYERS_POINTS[i]}\n")
+            print(f"{players_list[0]}.{players_list[1]}: {PLAYERS_POINTS[i]}\n")
+        self.player_data = [players_list[1], PLAYERS_POINTS[i]]
+        return self.player_data
+            
--- data_adding_program.py
+++ data_adding_program.py
@@ -1,4 +1,7 @@
 import csv
+from core import GameCore
+from main_functions import PLAYERS_POINTS
+
 
 # Lista danych
 player = ["\U0001F535", "chomik", 12]
@@ -8,30 +11,37 @@ player3 = ['bl', 'kamele', 20]
 player4 = ['blu', 'kamn', 39]
 
 # Funkcja do zapisania danych do pliku CSV (na początek)
-def write_to_csv():
-    with open('ranking.csv', 'a', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(["Kolor", "Nazwa", "Wartość"])  # Nagłówki
-        writer.writerow(player)  # Dodajemy dane
-        writer.writerow(player1)
-        writer.writerow(player2)
-        writer.writerow(player3)
-        writer.writerow(player4)
+class CsvWriter(GameCore):
+    def __init__(self):
+        super().__init__()
+        self.game_core = GameCore()
 
-# Funkcja sortująca dane w pliku CSV
-def sort_csv_by_column():
-    # Wczytaj dane z pliku CSV
-    with open('ranking.csv', 'r', encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-        data = list(reader)  # Konwertuj na listę słowników
-    
-    # Posortuj dane według kolumny 'Wartość' (należy przekształcić na int)
-    sorted_data = sorted(data, key=lambda row: int(row['Wartość']), reverse=True)  # Sortowanie malejąco
-    
-    # Zapisz posortowane dane z powrotem do pliku CSV
-    with open('ranking.csv', 'w', newline='', encoding='utf-8') as file:
-        writer = csv.DictWriter(file, fieldnames=reader.fieldnames)
-        writer.writeheader()  # Zapisz nagłówki kolumn
-        writer.writerows(sorted_data)  # Zapisz posortowane wiersze
-write_to_csv()
-sort_csv_by_column()
+    def write_to_csv(self):
+        with open('ranking.csv', 'a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Nazwa", "Wynik"])  # Nagłówki
+            writer.writerow(self.game_core.player_data)  # Dodajemy dane
+
+    # Funkcja sortująca dane w pliku CSV
+    def sort_csv_by_column(self):
+        # Wczytaj dane z pliku CSV
+        with open('ranking.csv', 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            data = list(reader)  # Konwertuj na listę słowników
+        
+        # Posortuj dane według kolumny 'Wartość' (należy przekształcić na int)
+        sorted_data = sorted(data, key=lambda row: int(row["Wynik"]), reverse=True)  # Sortowanie malejąco
+        
+        # Zapisz posortowane dane z powrotem do pliku CSV
+        with open('ranking.csv', 'w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=reader.fieldnames)
+            writer.writeheader()  # Zapisz nagłówki kolumn
+            writer.writerows(sorted_data)  # Zapisz posortowane wiersze
+
+
+
+
+
+    write_to_csv()
+    sort_csv_by_column()
+
